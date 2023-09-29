90fee634c5a809a1b37ab07340df4c023bdee56b
Anne Jerusalem <aliese.jeru@web.de>
2023-09-29 11:43:38 +0200

    Aufgabe erledigt

--- js/index.js
+++ js/index.js
@@ -8,10 +8,13 @@ console.clear();
   - the number of sales
 */
 
-// --v-- write your code here --v--
+// --v-- write your code here -,-v--
 
 // --^-- write your code here --^--
-
+const title = "the Lord of the Javascript";
+const author = "Mario";
+let rating = 4.2;
+let numberOfSales = 120;
 /*
 2: Log all variables to the console, for example:
 
@@ -20,13 +23,19 @@ Author: Mario
 Rating: 4.2
 Sales: 120
 
+bookTitle = "The Lord of the Javascript";
+console.log()
+
 Increase the number of sales and change the rating of the book afterwards.
 Then log all variables to the console again.
 */
-
+numberOfSales++;
 // --v-- write your code here --v--
 
 // --^-- write your code here --^--
+
+numberOfSales++;
+rating++;
 
 /*
 3: This code is quite repetitive and hard to read.
@@ -39,3 +48,11 @@ Then log all variables to the console again.
 // --v-- write your code here --v--
 
 // --^-- write your code here --^--
+console.log("hier ist die super Funktion");
+function logBookData() {
+  console.log("Title");
+  console.log(
+    `Title = ${title} ; Author = ${author} ; Rating = ${rating} ; numberOfSales = ${numberOfSales} `
+  );
+}
+logBookData();
